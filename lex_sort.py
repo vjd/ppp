4079f3c7276171bd7a85f815df55f0fad4e1001f
Vijay Ivaturi <vivaturi@rx.umaryland.edu>
2014-10-08 06:03:45 -0400

    second iteration - function specification using if __name__ convention. 4 items still #TODO

--- lex_sort.py
+++ lex_sort.py
@@ -2,23 +2,47 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Lexicographic sort.')
 parser.add_argument("infile", help="A text based input file with .txt extension", type=argparse.FileType('r'))
-parser.add_argument("outfile", help="A text base output file with .txt extension",type=argparse.FileType('w'))
-parser.add_argument("-i", "--insensitive", action="store_true",help="case insensitive lexicographic sort")
-parser.add_argument("-nl", "--nline", type=int, help="number of lines to print in output", default=100)
+parser.add_argument("outfile", help="A text base output file with .txt extension", type=argparse.FileType('w'))
+parser.add_argument("-i", "--insensitive", action="store_true", help="case insensitive lexicographic sort")
+parser.add_argument("-n", "--nline", type=int, help="number of lines to print in output", default=100)
+# TODO: How to set the default max for --nline to be based on the length of the input file?
 parser.add_argument("-p", "--printout", type=str, help="print result on console", default="no",
-                        choices=['y', 'Y', 'yes', 'YES', 'n', 'no', "N", "NO"])
+                    choices=['yes', 'no'])
 
 args = parser.parse_args()
-f=args.infile
-lines=f.read()
-f.close()
-if args.insensitive:
-    lexsort=sorted(lines,key=lambda y:y.lower())
-else:
-    lexsort=sorted(lines)
-o=args.outfile
-o.write(str(lexsort))
-o.close()
-if args.printout =="yes":
-    print(lexsort)
-# TODO: fix formatting of output and then work on -nl
+
+
+def lex(infile, outfile):
+    '''
+    This program performs a simple lexicographic sort
+    :param infile: input text based file
+    :param outfile: output file with sorted text
+    :return: outfile and optional print on the console
+    '''
+    clean_lex_sort = []
+    to_sort = args.infile
+    lines = to_sort.readlines()
+    to_sort.close()
+    if args.insensitive:  # add check to make sure its a string
+        lex_sort = sorted(lines, key=lambda y: y.lower())
+    else:
+        lex_sort = sorted(lines)
+    for i in range(len(lex_sort)):
+        clean_lex_sort.append(lex_sort[i].strip('\n'))
+    #clean_lex_sort = '/n'.join(clean_lex_sort)
+    out_sort = args.outfile
+    out_sort.write(str(clean_lex_sort))
+    out_sort.close()
+    print(lines)
+    #nlines_to_print = args.nline
+
+    if args.printout == 'yes':
+        for i in clean_lex_sort:
+            print(i)
+
+# TODO: Learn how to format the list in outfile to print on a newline
+# TODO: To print number of lines requested by user using --nline
+# TODO: Type check for options with informative error messages in case of user entry error
+
+if __name__ == "__main__":
+    lex(infile=args.infile, outfile=args.outfile)
